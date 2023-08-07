e191ae1bd5fe52b421256415aaf43ca6c0ff1555
Frozen <1884084+xrdavies@users.noreply.github.com>
2023-08-07 12:04:38 +0800

    add twitter oauth

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,22 +3,26 @@ import express from "express";
 import axios from "axios";
 import cors from "cors";
 
-// twitter Config
+/// twitter Config
 const BearerToken = process.env.TWITTER_BEARER_TOKEN;
+const ClientID = process.env.TWITTER_APP_CLIENT_ID;
+const ClientSecret = process.env.TWITTER_APP_CLIENT_SECRET;
 
 const port = 8888;
 const app = express();
 
 app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get('/getTwitterID/:userName', function (req, res) {
+app.get("/api/twitter/get_id/:userName", function (req, res) {
     var params = req.params;
     console.debug("=====", params.userName)
     axios({
-        method: 'get',
+        method: "get",
         url: `https://api.twitter.com/2/users/by?usernames=${params.userName}`,
         headers: {
-            accept: 'application/json',
+            accept: "application/json",
             Authorization: `Bearer ${BearerToken}`
         }
     }).then((dataResult) => {
@@ -29,9 +33,46 @@ app.get('/getTwitterID/:userName', function (req, res) {
 })
 
 
-app.get('/alive', function (req, res) {
+app.post("/auth_callback/twitter", function (req, res) {
     var params = req.params;
-    res.send('ok');
+    console.debug("=====", params)
+
+
+    var body = req.body;
+    console.debug("======body", body);
+
+
+    // todo: add ethereum wallet signature from client
+
+    var formdata = {
+        "client_id": ClientID,
+        "client_secret": ClientSecret,
+        "grant_type": "authorization_code",
+        "code_verifier": "challenge",
+        "code": req.body.code,
+        "redirect_uri": req.body.redirect_uri,
+    }
+
+    axios({
+        method: "post",
+        url: `https://api.twitter.com/2/oauth2/token`,
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "Accept": "application/json",
+            "Authorization": `Bearer ${BearerToken}`
+        },
+        data: axios.toFormData(formdata)
+    }).then((dataResult) => {
+        res.send(dataResult.data);
+    }).catch((error) => {
+        res.status(500).send(error);
+    });
+})
+
+
+app.get("/alive", function (req, res) {
+    var params = req.params;
+    res.send("ok");
 })
 
 app.listen(port, () => {
